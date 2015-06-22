bf83f0b4f394ab6d5592c1083c3a55d5a26417f6
leo108 <leo108@qq.com>
2015-06-22 10:35:35 +0800

    exe name

--- run.sh
+++ run.sh
@@ -14,4 +14,4 @@ sed -i "s/{REDIS_HOST}/${REDIS_PORT_6379_TCP_ADDR}/g" $confFile \
 && sed -i "s/{REDIS_PORT}/${REDIS_PORT_6379_TCP_PORT}/g" $confFile 
 
 cd $GOPATH/src/github.com/dinp/gorouter/
-./router
+./gorouter
